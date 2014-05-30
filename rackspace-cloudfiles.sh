2526233fbf09ee88c868c5fdf17473f17bb3d3f5
Michael Ryvkin <mpryvkin@users.noreply.github.com>
2014-05-29 23:00:27 -0400

    Updated the description

--- rackspace-cloudfiles.sh
+++ rackspace-cloudfiles.sh
@@ -1,34 +1,17 @@
 #!/bin/bash
 
 #
-# DESCRIPTION
+# rackspace-cloudfiles.sh
+# =======================
 #
-# rackspace-cloudfiles.sh is a bash script that uses curl to interact with 
-# Rackspace CloudFiles containers/objects using Rackspace Cloud Identity API 
-# v1.1. 
+# rackspace-cloudfiles.sh is a bash script that uses curl to interact with
+# Rackspace CloudFiles containers/objects using Rackspace Cloud Identity API
+# v1.1.
 #
-# AUTHOR
-#
-# Michael Ryvkin <mryvkin@gyrocode.com>
-#
-# COPYRIGHT
-#
-# Copyright (c) 2013 Michael Ryvkin <mryvkin@gyrocode.com>
-#
-# LICENSE
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Author: Michael Ryvkin, http://www.gyrocode.com
+# License: GNU General Public License, http://www.gnu.org/licenses/gpl.html
+# Created: 11/03/2013
+# Last updated: 05/29/2014
 #
 
 
